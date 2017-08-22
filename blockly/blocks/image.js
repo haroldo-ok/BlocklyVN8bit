d80eda426301de1cd6fe3b78365b04fb224ee7c9
Haroldo de Oliveira Pinheiro <haroldoop@gmail.com>
2017-08-21 23:25:24 -0300

    Read images from project dir

--- blockly/blocks/image.js
+++ blockly/blocks/image.js
@@ -6,12 +6,22 @@ Blockly.Blocks['demo_image'] = {
 	* @this Blockly.Block
 	*/
 	init: function() {
-		//var config = require('./config');
+		console.log(__dirname);
 		
-		var options = [
-			['no', 'https://neil.fraser.name/common/no.gif'],
-			['yes', 'https://neil.fraser.name/common/yes.gif']
-		];
+		const fs = require('fs');
+		const path = require('path');
+		const config = require('../../config');
+		
+		var dir = config.dir('projects');
+		var options = fs.readdirSync(dir).reduce((list, name) => {
+			let extName = path.extname(name);
+			if (/^\.(png|jpg|jpeg)$/.test(extName)) {
+				var onlyFName = path.basename(name, extName);
+				list.push([onlyFName, path.resolve(dir, name)]);
+			}
+			return list;
+		}, []);
+		
 		var dropdown = new Blockly.FieldDropdown(options,
 		function(newOp) {
 			this.sourceBlock_.getField('IMAGE').setValue(newOp);
