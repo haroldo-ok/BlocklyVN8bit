1c12ea51f1a83092a4dc1a345acb41fe7106bedc
Haroldo de Oliveira Pinheiro <haroldoop@gmail.com>
2017-08-21 22:47:47 -0300

    Image component test
    Based on https://groups.google.com/forum/#!topic/blockly/Su9HYUXS_K8

---
+++ blockly/blocks/image.js
@@ -0,0 +1,23 @@
+'use strict'
+
+Blockly.Blocks['demo_image'] = {
+	/**
+	* Demo block for image changing.
+	* @this Blockly.Block
+	*/
+	init: function() {
+		var options = [
+			['no', 'https://neil.fraser.name/common/no.gif'],
+			['yes', 'https://neil.fraser.name/common/yes.gif']
+		];
+		var dropdown = new Blockly.FieldDropdown(options,
+		function(newOp) {
+			this.sourceBlock_.getField('IMAGE').setValue(newOp);
+		});
+		this.appendDummyInput()
+			.appendField(dropdown, 'SOURCE')
+			.appendField(new Blockly.FieldImage('', 11, 11, '*'), 'IMAGE');
+		this.getField('IMAGE').EDITABLE = true;
+		this.setColour(20);
+	}
+};
