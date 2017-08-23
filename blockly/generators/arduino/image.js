563e231de301575e193f649accb5c03233eebdd4
Haroldo de Oliveira Pinheiro <haroldoop@gmail.com>
2017-08-22 21:59:54 -0300

    Generate code for portraits.

--- blockly/generators/arduino/image.js
+++ blockly/generators/arduino/image.js
@@ -2,18 +2,28 @@
 
 (function(){
 	
-Blockly.Arduino.background_image = function() {
-	let source = this.getField('SOURCE');
+function getImage(component) {
+	let source = component.getField('SOURCE');
 	
 	let imgName = source.getText();
 	Blockly.Arduino.images_[imgName] = {
 		vgDecl: 'extern uint16 vg_' + imgName + '[];\n',
 		viDecl: 'const uint16 *vi_' + imgName + ' = vg_' + imgName + ';'
 	}
+
+	return 'vi_' + imgName;
+}
 	
-	let code = 'vnScene(vi_' + imgName + ');\n';
+Blockly.Arduino.background_image = function() {
+	let varName = getImage(this);
+	let code = 'vnScene(' + varName + ');\n';
 	return code;
 };
 
+Blockly.Arduino.portrait_image = function() {
+	let varName = getImage(this);
+	let code = 'vnShow(' + varName + ');\n';
+	return code;
+};
 	
 })();
