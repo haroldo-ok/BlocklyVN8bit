a7327d82b6252d6c6c701548d47523c1ef147100
Haroldo de Oliveira Pinheiro <haroldoop@gmail.com>
2017-08-23 22:35:55 -0300

    Turned execution into a Promise.

--- blockly/apps/blocklyduino/vn32x.js
+++ blockly/apps/blocklyduino/vn32x.js
@@ -2,11 +2,10 @@ const cmd = require('node-cmd');
 
 const config = require('./config');
 
-
-module.exports = {
-	make: () => {
-		const bat = config.fileName('vn32x', 'exec.bat make');
-		const processRef = cmd.get(bat, () => console.log('Done.'));
+function exec(commandLine) {
+	return new Promise((resolve, reject) => {
+		const bat = config.fileName('vn32x', 'exec.bat ' + commandLine);
+		const processRef = cmd.get(bat, () => resolve());
 		
 		// listen to the terminal output
 		let data_line = '';
@@ -19,5 +18,9 @@ module.exports = {
 				}
 			}
 		);
-	}
+	});	
+}
+
+module.exports = {
+	make: () => exec('make')
 }
