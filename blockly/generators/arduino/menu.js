65d5e04c5d98d2716ea5ac5f1f0af7182f4e6f93
Haroldo de Oliveira Pinheiro <haroldoop@gmail.com>
2017-08-26 14:29:21 -0300

    Generated menu option bodies.

--- blockly/generators/arduino/menu.js
+++ blockly/generators/arduino/menu.js
@@ -16,6 +16,11 @@ function getVariables() {
 	var varStack = getVarStack();
 	return varStack[varStack.length - 1];
 }
+
+function getMenuOptionCodes() {
+	Blockly.Arduino.menuContexts_.menuOptionCodes = Blockly.Arduino.menuContexts_.menuOptionCodes || {};
+	return Blockly.Arduino.menuContexts_.menuOptionCodes;
+}
 	
 Blockly.Arduino.menu = function() {	
 	const menuNumber = nextMenuNumber();		
@@ -26,19 +31,28 @@ Blockly.Arduino.menu = function() {
 	const internalCode = Blockly.Arduino.statementToCode(this, 'DO');
 	getVarStack().pop();
 	
+	const optionBodies = variables.slice(1).map(menuVar => getMenuOptionCodes()[menuVar]);
+	
 	return '{\n' + 
 		'  int ' + variables.join(', ') + ';\n\n' +
 		internalCode + '\n' +
-		'  ' + varName + ' = vnMenu();\n' +
-		'}';
+		'  ' + varName + ' = vnMenu();\n\n' +
+		Blockly.Arduino.prefixLines(optionBodies.join('\n\n'), Blockly.Arduino.INDENT) + 
+		'\n}\n';
 };
 
 Blockly.Arduino.menu_option = function() {
 	const optionNumber = nextMenuNumber();
 	const varName = 'mn_choice_' + optionNumber;
 	const text = this.getFieldValue('TEXT');
+	const internalCode = Blockly.Arduino.statementToCode(this, 'DO');
 	
 	getVariables().push(varName);
+
+	const optionCode = 'if (' + getVariables()[0] + ' == ' + varName + ') {\n' +
+	internalCode + '\n}';
+	
+	getMenuOptionCodes()[varName] = optionCode;
 	
 	return varName + ' = addMenuItem(' + Blockly.Arduino.quote_(text) + ');\n';
 }
