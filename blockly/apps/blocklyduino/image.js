639430805a676ec1dd3149987ef5cb2fc253948e
Haroldo de Oliveira Pinheiro <haroldoop@gmail.com>
2017-08-28 21:58:43 -0300

    Display the stretched image to the user.

--- blockly/apps/blocklyduino/image.js
+++ blockly/apps/blocklyduino/image.js
@@ -1,7 +1,7 @@
 const $ = require('jquery');
 const project = require('./project');
 
-rivets.bind(require('jquery')('#content_backgrounds'), {
+rivets.bind($('#content_backgrounds'), {
 	data: {
 		imageName: 'none',
 		images: project.backgrounds		
@@ -11,6 +11,14 @@ rivets.bind(require('jquery')('#content_backgrounds'), {
 		selectFile: function(ev, model) {
 			console.log('selectFile', this, arguments);
 			model.data.imageName = ev.target.files[0].name;
+			
+			let img = new Image();
+			img.onload = () => {
+				let canvas = $(ev.target).parent().find('> canvas')[0];
+				let ctx = canvas.getContext('2d');
+				ctx.drawImage(img, 0, 0, canvas.width, canvas.height);				
+			}
+			img.src = ev.target.files[0].path;
 		},
 		
 		addImage: function(ev, model) {
