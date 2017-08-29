b69fe5055e8e241734aa2847580b5321d60576c1
Haroldo de Oliveira Pinheiro <haroldoop@gmail.com>
2017-08-28 21:34:48 -0300

    Added buttons to add images.

--- blockly/apps/blocklyduino/image.js
+++ blockly/apps/blocklyduino/image.js
@@ -2,6 +2,19 @@ const $ = require('jquery');
 const project = require('./project');
 
 rivets.bind(require('jquery')('#content_backgrounds'), {
-	something: 'Testing',
-	images: project.backgrounds
+	data: {
+		imageName: 'none',
+		images: project.backgrounds		
+	},
+	
+	controller: {
+		selectFile: function(ev, model) {
+			console.log('selectFile', this, arguments);
+			model.data.imageName = ev.target.files[0].name;
+		},
+		
+		addImage: function(ev, model) {
+			console.log('addImage', this, arguments);
+		}
+	}
 });
