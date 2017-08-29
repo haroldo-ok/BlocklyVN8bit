f34d6a68360e776477230f67a5c239bf6a2a99d7
Haroldo de Oliveira Pinheiro <haroldoop@gmail.com>
2017-08-28 21:00:20 -0300

    List images

---
+++ blockly/apps/blocklyduino/image.js
@@ -0,0 +1,7 @@
+const $ = require('jquery');
+const project = require('./project');
+
+rivets.bind(require('jquery')('#content_backgrounds'), {
+	something: 'Testing',
+	images: project.backgrounds
+});
