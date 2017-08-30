30bb6d7759e0f8a57159811542e916812e2f9e0c
Haroldo de Oliveira Pinheiro <haroldoop@gmail.com>
2017-08-29 23:31:52 -0300

    Different resize logic for bgs and portraits

--- blockly/apps/blocklyduino/image.js
+++ blockly/apps/blocklyduino/image.js
@@ -3,6 +3,9 @@ const project = require('./project');
 const canvasBuffer = require('electron-canvas-to-buffer');
 const fs = require('fs');
 
+const MAX_WIDTH = 320;
+const MAX_HEIGHT = 202;
+
 function prepareImageList(options) {
 	let $container = $(options.selector);
 	let imageAccessor = project[options.origin];
@@ -25,7 +28,7 @@ function prepareImageList(options) {
 					let canvas = model.data.canvas;
 					let ctx = canvas.getContext('2d');				
 					ctx.clearRect(0, 0, canvas.width, canvas.height);
-					ctx.drawImage(img, 0, 0, canvas.width, canvas.height);				
+					options.prepareImage(img, canvas, ctx);
 				}
 				img.src = ev.target.files[0].path;
 			},
@@ -41,11 +44,35 @@ function prepareImageList(options) {
 [
 	{
 		selector: '#content_backgrounds',
-		origin: 'bg'
+		origin: 'bg',
+		prepareImage: (img, canvas, ctx) => {
+			canvas.width = MAX_WIDTH;
+			canvas.height = MAX_HEIGHT;
+			ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+		}
 	},
 
 	{
 		selector: '#content_portraits',
-		origin: 'portrait'
+		origin: 'portrait',
+		prepareImage: (img, canvas, ctx) => {			
+			// The width must be a multiple of 8.
+			let targetWidth = Math.floor((img.width + 7) / 8) * 8;
+			
+			canvas.width =  Math.min(targetWidth, MAX_WIDTH);
+			canvas.height = Math.min(img.height, MAX_HEIGHT);
+			
+			// Calculate image size to fit
+			let imgWidth = img.width;
+			let imgHeight = img.height;			
+			if (imgHeight > canvas.height) {
+				imgWidth = Math.round(imgWidth * canvas.height / imgHeight);
+				imgHeight = canvas.height;
+			}
+			
+			// Center horizontally
+			let x = Math.round((canvas.width - imgWidth) / 2);
+			ctx.drawImage(img, x, 0, imgWidth, imgHeight);
+		}
 	}
 ].forEach(prepareImageList);
