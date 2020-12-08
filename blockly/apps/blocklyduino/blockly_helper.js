2340d367bcb4ac7332a1b5cd3e58c8f96cf0e6c5
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2020-12-08 19:47:20 -0300

    Method to create the required dirs, if absent.

--- blockly/apps/blocklyduino/blockly_helper.js
+++ blockly/apps/blocklyduino/blockly_helper.js
@@ -1,4 +1,8 @@
 const { fileName } = require('./config');
+const fs = require('fs');
+const path = require('path');
+const config = require('./config');
+const project = require('./project');
 
 /**
  * Execute the user's code.
@@ -119,32 +123,38 @@ function saveCode() {
 
 }
 
-function generateCode() {
-	const fs = require('fs');
-	const path = require('path');
-	const config = require('./config');
-	const project = require('./project');
+const makeDirIfNotExists = async filePath => {
+	return new Promise((resolve, reject) => {
+		fs.mkdir(filePath, err => {
+			// Ignore "Directory already exists" errors
+			if (err && err.code != 'EEXIST') {
+				console.log(`Error creating dir ${filePath}`, err);
+				reject(err);
+				return;
+			}
 
-	const makeDirIfNotExists = async filePath => {
-		return new Promise((resolve, reject) => {
-			fs.mkdir(filePath, err => {
-				// Ignore "Directory already exists" errors
-				if (err && err.code != 'EEXIST') {
-					console.log(`Error creating dir ${filePath}`, err);
-					reject(err);
-					return;
-				}
-
-				resolve(fileName);
-			});
+			resolve(fileName);
 		});
-	}
+	});
+}
 
+const targetPath = () =>  config.fileName('8bitUnity', 'projects/' + project.current.name + '/');
+
+const createTargetDirectories = async () => {
+	const targetPath = config.fileName('8bitUnity', 'projects/' + project.current.name + '/');
+	const subDirs = ['bitmaps', 'chunks', 'music', 'sprites', 'src']
+		.map(name => `${targetPath}${name}/`);
+
+	await makeDirIfNotExists(targetPath);
+	await Promise.all(subDirs.map(dir => makeDirIfNotExists(dir)));
+}
+
+function generateCode() {
 	function writeGeneratedFile(fileName, content) {
 		return new Promise(async (resolve, reject) => {
 			const filePath = config.fileName('8bitUnity', 'projects/' + project.current.name + '/');
 
-			await makeDirIfNotExists(filePath); 
+			await createTargetDirectories(); 
 			
 			fs.writeFile(filePath + fileName, content, function(err) {
 				if(err) {
